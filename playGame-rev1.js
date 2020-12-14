078c77ed7184ba42fd572da60d49924a2dcc1dac
Rivo Wowor <rivowowor7@gmail.com>
2020-12-14 15:13:10 +0800

    Added text box and start game mechanics
    Text box is used for game status like what hand you and the bot playing,
    the score, etc. And start game mechanics means that you can define how
    many rounds you want to play and click the start game buttons will
    enable the game buttons (rock paper and scissors btn) and let you play
    it.
    
    im crying rn

--- playGame-rev1.js
+++ playGame-rev1.js
@@ -1,11 +1,15 @@
-
 // Defining rounds var
 let currentRounds = 0;
-let maxRounds = 5;
+let maxRounds = 0;
 
 // Defining scores
 let playerScore = 0;
 let computerScore = 0;
+
+// Defining div for disabling input
+const btnStart = document.getElementById('gameStart');
+const inptRounds = document.getElementById('gameRounds');
+
 
 // Defining player buttons
 const playerRock = document.getElementById('playerSel-rock');
@@ -17,7 +21,30 @@ playerRock.addEventListener("click", () => playRound('rock') );
 playerPaper.addEventListener("click", () => playRound('paper'));
 playerScissors.addEventListener("click", () => playRound('scissors'));    
 
+document.querySelector("body").onload = () => disableButtons();
 
+function disableButtons() {
+    btnStart.disabled = false;
+    inptRounds.disabled = false;
+    
+    playerRock.disabled = true;
+    playerPaper.disabled = true;
+    playerScissors.disabled = true
+}
+
+function startGame() {
+    maxRounds = document.getElementById('gameRounds').value;
+    btnStart.disabled = true;
+    inptRounds.disabled = true;
+    
+    playerRock.disabled = false;
+    playerPaper.disabled = false;
+    playerScissors.disabled = false;
+
+    clearMsg();
+    console.clear();
+
+}
 // Create a function called computerPlay to make a bot (literally)
 function computerPlay() {
     // Create a switch condition with random number generator on it's expression
@@ -31,17 +58,15 @@ function computerPlay() {
     }
 }
 
-/* Create a function called playRound the function should take two parameters
-   playerSelection and computerSelection
+/* Create a function called playRound 
+   the function should take a parameters called playerSelection 
    and should return the results */
 
 function playRound(playerSelection) {
     // Add round
     currentRounds++;
     console.log("Round ", currentRounds);
-
-    // Declare a roundWinner var
-    let roundWinner = "";
+    updateMsg(`Round ${currentRounds}`);
 
     // Play the bot
     let computerSelection = computerPlay();
@@ -50,23 +75,28 @@ function playRound(playerSelection) {
     console.log("Player played: ", playerSelection);
     console.log("Computer played: ", computerSelection);
 
+    // Write into gameMsg
+    updateMsg(`Player played: ${playerSelection}`);
+    updateMsg(`Computer played: ${computerSelection}`);
+
     switch(true) {
         // IF playerSelection and computerSelection have same value, then it's a tie
         case playerSelection === computerSelection: {
             console.log("Tie");
+            updateMsg("Tie", "bold");
             break;
         }
         // player wins condition
         case ((playerSelection == "paper") && (computerSelection == "rock")) || ((playerSelection == "rock") && (computerSelection == "scissors")) || ((playerSelection == "scissors") && (computerSelection == "paper")): {
-            roundWinner = "player";
-            console.log("Player Wins");
+            console.log("Player Wins!");
+            updateMsg("Player Wins!", "bold")
             playerScore++;
             break;
         }
         // computer wins condition
         case ((playerSelection == "rock") && (computerSelection == "paper")) || ((playerSelection == "scissors") && (computerSelection == "rock")) || ((playerSelection == "paper") && (computerSelection ==  "scissors")): {
-            roundWinner = "computer";
             console.log("Computer Wins");
+            updateMsg("Computer Wins!", "bold")
             computerScore++;
             break;
         }
@@ -76,7 +106,35 @@ function playRound(playerSelection) {
 
     // Line break
     console.log("---")
+    updateMsg("---")
     checkFinished();
+}
+
+// Update message in Textbox so we can play in HTML instead of console.log
+function updateMsg(msg, param1){
+    let para = document.createElement("p");
+    if (param1) {
+        para.style.fontWeight = param1;
+    }
+    let node = document.createTextNode(msg);
+    para.appendChild(node);
+
+    let element = document.getElementById("gameMsg");
+    element.appendChild(para);
+    updateScroll();
+}
+
+function clearMsg() {
+    const myNode = document.getElementById("gameMsg");
+    while (myNode.firstChild) {
+      myNode.removeChild(myNode.lastChild);
+    }
+}
+
+// For auto scrolling to bottom of the div
+function updateScroll(){
+    let element = document.getElementById("gameMsg");
+    element.scrollTop = element.scrollHeight;
 }
 
 function checkFinished() {
@@ -85,19 +143,40 @@ function checkFinished() {
     }
 }
 
+// Announce the winner
 function winner() {
     console.log("winner() function is executed!")
-    // Report the score
+
+    // Report the score in console
     console.log("Player scored ", playerScore, " point(s)");
     console.log("Computer score ", computerScore, " point(s)");
 
+    // Report the score in div
+    updateMsg(`Player scored ${playerScore} point(s)`);
+    updateMsg(`Computer scored ${computerScore} point(s)`);
+
     // Determine the winner
     switch (calculateScore(playerScore, computerScore)) {
-        case "player": console.log("Player Wins!"); break;
-        case "computer": console.log("Computer Wins!"); break;
-        case "tie": console.log("It's a Tie!"); break;
+        case "player": {
+            console.log("Player Wins!");
+            updateMsg(`Player Wins!`, "bold")
+            break; 
+        }
+        case "computer": {
+            console.log("Computer Wins!");
+            updateMsg(`Computer Wins!`, "bold")
+            break; 
+        }
+        case "tie": {
+            console.log("It's a Tie!");
+            updateMsg(`It's a Tie!`, "bold")
+            break; 
+        }
         default: console.log("Something wrong"); break;
     }
+
+    // Disable game buttons
+    disableButtons()
 }
 
 // Create function to calculate score based on player and computer scores
@@ -117,56 +196,4 @@ function calculateScore(playerScore, computerScore) {
     } else {
         return "unknown";
     }
-}
-
-// Create a function of the game itself
-function game(rounds) {
-    // Hide startpage div
-    document.getElementsByClassName('startpage')[0].style.visibility = "hidden";
-    // Create 2 variables to store score for each player
-
-    
-    // Create for loop including the playRound function
-    for (i = 1; i <= rounds; i++) {
-        // Print the round
-        
-        // Run playRound function
-        // Prompt player for a input
-        console.log("Player played: ", playerSelection);
-        console.log("Computer played: ", computerSelection);
-        
-        
-        // Add score using IF statement
-        if (winner === "player") {
-            console.log("Player Wins");
-            playerScore++;
-        } else if (winner === "computer") {
-            console.log("Computer Wins");
-            computerScore++;
-        } else if (winner === "tie") {
-            console.log("Tie");
-        } else {
-            console.log("Unknown value detected");
-            i--;
-        }
-        // Add line break
-        console.log("---")
-    }
-
-    /*
-    MOVE THIS CODE TO winner() FUNCTION
-
-    // Report the score
-    console.log("Player scored ", playerScore, " point(s)");
-    console.log("Computer score ", computerScore, " point(s)");
-
-    // Determine the winner
-    switch (calculateScore(playerScore, computerScore)) {
-        case "player": console.log("Player Wins!"); break;
-        case "computer": console.log("Computer Wins!"); break;
-        case "tie": console.log("It's a Tie!"); break;
-        default: console.log("Something wrong"); break;
-    }
-    */
-
 }
